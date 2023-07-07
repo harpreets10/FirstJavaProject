eb8ee4f0c291ec2d9a2e0e0cca653cdbf699802b
harpreets10 <kharpreet424@gmail.com>
2023-07-06 22:26:08 -0700

    First Commit

---
+++ src/firstPackage/MobileMain.java
@@ -0,0 +1,18 @@
+package firstPackage;
+
+public class MobileMain {
+
+	public static void main(String[] args) {
+	MobilePhone iPhone=new MobilePhone();
+	iPhone.brandName= "iphone";
+	iPhone.modelNumber= 12;
+	iPhone.screenSize=6;
+	iPhone.storageCapcity=6;
+	iPhone.isCameraPresent=true;
+	iPhone.makeCalls();
+	iPhone.sendTextMessage();
+	
+
+	}
+
+}
---
+++ src/firstPackage/MobilePhone.java
@@ -0,0 +1,18 @@
+package firstPackage;
+
+public class MobilePhone {
+	String brandName;
+	int modelNumber;
+	int storageCapcity;
+	int screenSize;
+	boolean isCameraPresent;
+	void makeCalls() {
+	System.out.println("making calls from the phone");	
+	}
+	void sendTextMessage() {
+	System.out.println("sending text messages");	
+	}
+	
+
+}
+
