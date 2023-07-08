6e579eb12264b910c1f63d1a962b42a4bcb123b3
harpreets10 <kharpreet424@gmail.com>
2023-07-07 23:51:24 -0700

    adding learnoperations package

---
+++ src/learnOperations/ArithmeticOperationMain.java
@@ -0,0 +1,13 @@
+package learnOperations;
+
+public class ArithmeticOperationMain {
+
+	public static void main(String[] args) {
+		ArithmeticOperations ao=new ArithmeticOperations();
+		ao.num1=5;
+		ao.num2=8;
+		ao.addtwonumbers();
+
+	}
+
+}
---
+++ src/learnOperations/ArithmeticOperations.java
@@ -0,0 +1,13 @@
+package learnOperations;
+
+public class ArithmeticOperations {
+	int num1;
+	int num2;
+	void addtwonumbers() {
+		int sum=num1+num2;
+		System.out.println("Addition of" + num1+ "and" + num2+ "is" + sum);
+		
+	}
+	
+
+}
