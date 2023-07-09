12aa58a5e7f86e1cded318942f6aecfccdae0c85
harpreets10 <kharpreet424@gmail.com>
2023-07-09 15:19:45 -0700

    adding new water Tank assignment

---
+++ src/waterTank/WaterTank.java
@@ -0,0 +1,28 @@
+package waterTank;
+
+public class WaterTank {
+	int capacity;
+	void WaterTankcapcity() {
+	while(capacity<=100) {
+	capacity+=10;
+	System.out.println("water tank filled"+ capacity);
+	
+	if(capacity==100){
+	System.out.println("water tank reach to the capacity of 100");
+	break;
+	}
+	}
+	}
+	
+	
+		
+	}
+	
+	
+
+
+
+	
+	
+
+
---
+++ src/waterTank/WaterTankMain.java
@@ -0,0 +1,17 @@
+package waterTank;
+
+public class WaterTankMain {
+
+	public static void main(String[] args) {
+	WaterTank bucket = new WaterTank();
+	bucket.capacity=0;
+    bucket.WaterTankcapcity();
+		
+			
+		
+		
+		
+
+	}
+
+}
